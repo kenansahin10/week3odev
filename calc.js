2f804856511703d7b71385cce7275aa2a87515a3
kenansahin <kenansahin803@gmail.com>
2021-02-25 14:55:01 +0200

    prepair

---
+++ calc.js
@@ -0,0 +1,16 @@
+const calc = (num1, num2, operator) => {
+    // ...
+}
+
+// console.log(calc(5, 2, 'p'));
+
+const output = {
+    islemsonucu: 10,
+    hata: [
+        'sifira bölünme hatasi',
+        'girdiginiz deger sayi',
+        'islem +-*/ olmalidir.'
+    ]
+}
+
+console.log(output);
