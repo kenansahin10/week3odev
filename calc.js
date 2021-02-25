7bc6f92b3566b6338448b05803b4032b9a091b0f
kenansahin <kenansahin803@gmail.com>
2021-02-25 17:34:08 +0200

    1

--- calc.js
+++ calc.js
@@ -1,16 +1,50 @@
-const calc = (num1, num2, operator) => {
-    // ...
+const calc = (num1, operator, num2) => {
+    let number1,number2;
+    let calcResult = { result:0,errors:[] }; 
+    number1 = Number(num1);
+    number2 = Number(num2);
+    if (isNaN(number1) || number1==null)
+    {
+        calcResult.result = 'Hesaplanamadı';
+        calcResult.errors.push(`ilk parametre sayı girilmedi!!!`);
+    }
+    else if (isNaN(number2) || number2==null)
+    {
+        calcResult.result = 'Hesaplanamadı';
+        calcResult.errors.push(`ikinci parametre sayı girilmedi!!!`);
+    }
+    else
+        switch (operator) {
+            case '+':
+                calcResult.result = number1 + number2;            
+                break;
+            case '-':
+                calcResult.result = number1 - number2;
+                break;
+            case '*':
+                calcResult.result = number1 * number2;
+                break;
+            case '/':
+                if (number2 == 0) {
+                    calcResult.errors.push('Sıfıra bölme hatası yapıldı!!!');
+                    calcResult.result = 'Tanımsız';
+                }
+                else calcResult.result = number1 / number2;
+                break;
+            default:
+                calcResult.result = 'Hesaplanamadı';
+                calcResult.errors.push('Operator yanlış girildi!!!')
+                break;        
+        }
+    return calcResult;    
 }
-
-// console.log(calc(5, 2, 'p'));
-
-const output = {
-    islemsonucu: 10,
-    hata: [
-        'sifira bölünme hatasi',
-        'girdiginiz deger sayi',
-        'islem +-*/ olmalidir.'
-    ]
-}
-
-console.log(output);
+//Hata donduren islemler
+console.log(calc('a','+','1'));
+console.log(calc('1','+','a'));
+console.log(calc('1','.','2'));
+console.log(calc('1','/','0'));
+//Hatasiz islemler
+console.log(calc('1','+','1'));
+console.log(calc('1','-','1'));
+console.log(calc('1','*','1'));
+console.log(calc('4','/','2'));
